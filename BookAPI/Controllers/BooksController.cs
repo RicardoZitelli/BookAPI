fac6297fac29252723bf7f49c46a365511638a9f
ricardo_zitelli <ricardo_zitelli@hotmail.com>
2021-11-12 02:13:04 -0300

    Adicionar arquivos de projeto.

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,68 @@
+﻿using BookAPI.Models;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly IBookRepository _bookRepositories;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepositories = bookRepository;
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepositories.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepositories.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepositories.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if(id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            await _bookRepositories.Update(book);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete = await _bookRepositories.Get(id);
+            if (bookToDelete == null)
+                return NotFound();
+
+            await _bookRepositories.Delete(bookToDelete.Id);
+            
+            return NoContent();
+        }
+
+    }
+}
